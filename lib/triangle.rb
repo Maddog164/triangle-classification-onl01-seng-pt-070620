7f2e465d5e74282a7099f3ece87c2b095bdff101
Denise Henderson <denise@henderson3.us>
2020-08-08 18:00:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,9 @@ class Triangle
 
   def kind
     #binding.pry
-    if (@side_1 <= 0 || @side_2 <= 0 || @side_3<= 0)
+    if (@side_1 <= 0 || @side_2 <= 0 || @side_3<= 0) || 
+      (@side_1 + @side_2 <= @side_3) || ()@side_2 + @side_3 <= @side_1) ||
+      (@side_1 + @side_3 <= @side_2)
       # begin
         raise TriangleError
       # rescue TriangleError => error
